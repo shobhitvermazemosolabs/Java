147156dfb05abe2b99bac27edc8d743adbc64cf5
Shobhit Verma <shobhit.verma@zemosolabs.com>
2022-01-25 02:59:44 +0530

    Updated the assignment 7 and done with the assignment 6

---
+++ src/assignment6/Vampire.java
@@ -0,0 +1,39 @@
+package assignment6;
+import java.util.Arrays;
+
+
+public class Vampire
+{
+    final static int START = 11, END = 1000;
+    public static void main(String[] args)
+    {
+        char[] kChar, checkChar;
+        String kStr, checkStr;
+        int k;
+        int count=0;
+        for(int i=START; i<END; i++) {
+            String start=String.valueOf(i);
+            int max=(int)Math.min(END,Math.pow(10,start.length()));
+            for(int i1=i; i1<max; i1++) {
+
+                k = i * i1;
+
+                kStr = Integer.toString(k);
+                checkStr = Integer.toString(i) + Integer.toString(i1);
+
+
+
+                kChar = kStr.toCharArray();
+                checkChar = checkStr.toCharArray();
+
+                Arrays.sort(kChar);
+                Arrays.sort(checkChar);
+
+                if(Arrays.equals(kChar, checkChar) && count<100) {
+                    count++;
+                    System.out.println(i + " * " + i1 + " = " + k);
+                }
+            }
+        }
+    }
+}
