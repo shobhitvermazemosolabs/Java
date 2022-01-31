fa84a813979882b62e9ece5e47ea33145dfd4d87
Shobhit Verma <shobhit.verma@zemosolabs.com>
2022-01-31 08:25:20 +0530

    Done with the asssignment 4

--- src/assignment4/Kyc.java
+++ src/assignment4/Kyc.java
@@ -1,19 +1,50 @@
 package assignment4;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Scanner;
 
 public class Kyc {
-    public static void getDates(String signup,String current, int row_index){
+    public static void getDates(String signup,String current, int row_index, String dateArray[][]){
         DateFormat dateFormat=new SimpleDateFormat("dd-mm-yyyy");
         Calendar signupDate=Calendar.getInstance();
         Calendar currentDate=Calendar.getInstance();
-        Calendar systemDate=Calendar.getInstance();
+        Calendar startDate=Calendar.getInstance();
         signupDate.clear();
         currentDate.clear();
-        systemDate.clear();
+        startDate.clear();
+
+        try{
+            signupDate.setTime(dateFormat.parse(signup /*string*/));
+            startDate.setTime(dateFormat.parse(signup /*string*/));
+            currentDate.setTime(dateFormat.parse(current /*string*/));
+
+            if(currentDate.get(Calendar.YEAR)-signupDate.get(Calendar.YEAR)==0){
+                dateArray[row_index][0]="No Range";
+            }
+
+            startDate.add(Calendar.YEAR,(currentDate.get(Calendar.YEAR)-signupDate.get(Calendar.YEAR)));
+            signupDate.add(Calendar.YEAR,(currentDate.get(Calendar.YEAR)-signupDate.get(Calendar.YEAR)));
+
+            signupDate.add(Calendar.DATE,30);
+            startDate.add(Calendar.DATE,-30);
+
+            if(currentDate.after(startDate)&&currentDate.before(signupDate)){
+                dateArray[row_index][0]=dateFormat.format(startDate.getTime());
+                dateArray[row_index][1]=dateFormat.format(signupDate.getTime());
+            }
+            else if(currentDate.after(startDate)){
+                dateArray[row_index][0]=dateFormat.format(startDate.getTime());
+                dateArray[row_index][1]=dateFormat.format(currentDate.getTime());
+            }
+            else{
+                dateArray[row_index][0]="no range";
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
     public static void main(String[] args) {
         Scanner scannerObject=new Scanner(System.in);
@@ -23,8 +54,15 @@ public class Kyc {
         for(int row_index=0;row_index<inputSize;row_index++){
             String signup=scannerObject.next();
             String current=scannerObject.next();
-            getDates(signup,current,row_index);
+            getDates(signup,current,row_index,dateArray);
         }
-
+        for(int row_index=0;row_index<inputSize;row_index++){
+            System.out.println(dateArray[row_index][0]);
+            if(dateArray[row_index][1]!=null){
+                System.out.println(dateArray[row_index][1]);
+            }
+        }
+        scannerObject.close();
+        return;
     }
 }
